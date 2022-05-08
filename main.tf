670d4db08c4d8f1fae6c4946b96de6c42ef313b3
computer19810 <computer19810@163.com>
2022-05-08 22:01:25 +0800

    VM

--- main.tf
+++ main.tf
@@ -11,60 +11,65 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+     resource_group {
+      prevent_deletion_if_contains_resources = false
+
+    }
+  }
 }
-resource "azurerm_resource_group" "nicktfrg" {
-    name = "NickResourceGroup"
+resource "azurerm_resource_group" "Zhttfrg" {
+    name = "ZhtResourceGroup"
     location = "eastasia"
     tags = {
         environment = "My Terraform Demo"
     }   
 }
-resource "azurerm_virtual_network" "nicktfnetwork" {
-    name                = "NickVnet"
+resource "azurerm_virtual_network" "Zhttfnetwork" {
+    name                = "ZhtVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "eastasia"
-    resource_group_name = "${azurerm_resource_group.nicktfrg.name}"
+    resource_group_name = "${azurerm_resource_group.Zhttfrg.name}"
 
     tags = {
         environment = "My Terraform Demo"
     }
 }
-resource "azurerm_subnet" "nicktfsubnet" {
-    name                 = "NickSubnet"
-    resource_group_name  = "${azurerm_resource_group.nicktfrg.name}"
-    virtual_network_name = "${azurerm_virtual_network.nicktfnetwork.name}"
+resource "azurerm_subnet" "Zhttfsubnet" {
+    name                 = "ZhtSubnet"
+    resource_group_name  = "${azurerm_resource_group.Zhttfrg.name}"
+    virtual_network_name = "${azurerm_virtual_network.Zhttfnetwork.name}"
     #address_prefix       = "10.0.2.0/24"
     address_prefixes     = ["10.0.2.0/24"]
 }
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.nicktfrg.name}"
+        resource_group = "${azurerm_resource_group.Zhttfrg.name}"
     }
 
     byte_length = 8
 }
-resource "azurerm_network_interface" "nicktfnic" {
-    name                = "NickNIC"
+resource "azurerm_network_interface" "Zhttfnic" {
+    name                = "ZhtNIC"
     location            = "eastasia"
-    resource_group_name = "${azurerm_resource_group.nicktfrg.name}"
-    #network_security_group_id = "${azurerm_network_security_group.nicktfnsg.id}"
+    resource_group_name = "${azurerm_resource_group.Zhttfrg.name}"
+    #network_security_group_id = "${azurerm_network_security_group.Zhttfnsg.id}"
 
     ip_configuration {
-        name                          = "NickNicConfiguration"
-        subnet_id                     = "${azurerm_subnet.nicktfsubnet.id}"
+        name                          = "ZhtNicConfiguration"
+        subnet_id                     = "${azurerm_subnet.Zhttfsubnet.id}"
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.nicktfpublicip.id}"
+        public_ip_address_id          = "${azurerm_public_ip.Zhttfpublicip.id}"
     }
 
     tags = {
         environment = "My Terraform Demo"
     }
 }
-resource "azurerm_storage_account" "nicktfstorageaccount" {
-    name                = "nicksa${random_id.randomId.hex}"
-    resource_group_name = "${azurerm_resource_group.nicktfrg.name}"
+resource "azurerm_storage_account" "Zhttfstorageaccount" {
+    name                = "zhtsa${random_id.randomId.hex}"
+    resource_group_name = "${azurerm_resource_group.Zhttfrg.name}"
     location            = "eastasia"
     account_replication_type = "LRS"
     account_tier = "Standard"
@@ -74,20 +79,20 @@ resource "azurerm_storage_account" "nicktfstorageaccount" {
     }
 }
 
-resource "azurerm_public_ip" "nicktfpublicip" {
-    name                         = "NickPublicIP"
+resource "azurerm_public_ip" "Zhttfpublicip" {
+    name                         = "ZhtPublicIP"
     location                     = "eastasia"
-    resource_group_name          = "${azurerm_resource_group.nicktfrg.name}"
+    resource_group_name          = "${azurerm_resource_group.Zhttfrg.name}"
     allocation_method            = "Dynamic"
 
     tags = {
         environment = "My Terraform Demo"
     }
 }
-resource "azurerm_network_security_group" "nicktfnsg" {
-    name                = "NickNetworkSecurityGroup"
+resource "azurerm_network_security_group" "Zhttfnsg" {
+    name                = "ZhtNetworkSecurityGroup"
     location            = "eastasia"
-    resource_group_name = "${azurerm_resource_group.nicktfrg.name}"
+    resource_group_name = "${azurerm_resource_group.Zhttfrg.name}"
 
     security_rule {
         name                       = "SSH"
@@ -105,17 +110,17 @@ resource "azurerm_network_security_group" "nicktfnsg" {
         environment = "My Terraform Demo"
     }
 }
-resource "azurerm_virtual_machine" "nicktfvm" {
+resource "azurerm_virtual_machine" "Zhttfvm" {
     
-  name                  = "NickVM"
+  name                  = "ZhtVM"
   location              = "eastasia"
-  resource_group_name   = "${azurerm_resource_group.nicktfrg.name}"
-  network_interface_ids = ["${azurerm_network_interface.nicktfnic.id}"]
+  resource_group_name   = "${azurerm_resource_group.Zhttfrg.name}"
+  network_interface_ids = ["${azurerm_network_interface.Zhttfnic.id}"]
   vm_size               = "Standard_DS1_v2"
 
 
   storage_os_disk {
-        name              = "NickOsDisk"
+        name              = "ZhtOsDisk"
         caching           = "ReadWrite"
         create_option     = "FromImage"
         managed_disk_type = "Premium_LRS"
@@ -130,13 +135,13 @@ resource "azurerm_virtual_machine" "nicktfvm" {
   
   os_profile {
         computer_name  = "phoenix"
-        admin_username = "nick"
+        admin_username = "Zht"
     }
   os_profile_linux_config {
         disable_password_authentication = true
         ssh_keys {
-            path     = "/home/nick/.ssh/authorized_keys"
-            key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDCM1KCsC8U2j8BOgnu+GIfEO6XSP8qescCA0G9PfOk4twaFsua+mg4b0mtqxC9jvSpjLHvyCUWpuCyOiYZcNlW2JPKIw7n5Te7hgwO4IUH/yyzyT3xyiiwDTVTn9c7+8At2AnrXQAAfVgDsJ585L07XNOYqL6yB72b8onklMH6VtnGNlFXk7IMdP2UYWLDI7GIbE6/E5Epsy1hHggYKrQ4GMfI62YYw8c/WUBJg+aXy0LQre5+ohFcAFveT/VYSQXPkN+a3Kq32iByqg4jD7jK23djAhCegtlEoNcQHXVyj/aKFJyO0os3eOl9ICHyEkyW9PapewiY9AUMjg7Y/jQF7VQSUIC2vvIpkzDRdOsD6QyNtg6N49fUc3BRgj7wJ0BflTCT15b4FObbXanvHzIcH5KODVwBa5bacH/GV/r5rxiGZSsNUehKYfl30lazVUYvBI8LgOG+hHDGHPUTlz/pkbLDkpSzHT17wZ4U3hGYkj5F8lKGpXKTE3AvlYONa9c= computer19810@163.com"
+            path     = "/home/Zht/.ssh/authorized_keys"
+            key_data = "****************************************************************************8"
         }
   }
   tags = {
